0e9a85ddffb76d3dca7d3f9056b57dbcdd61e87f
Simon Bouchard <bouchards@amotus.ca>
2020-12-01 09:59:52 -0500

    solve day 1

---
+++ 01/solve.py
@@ -0,0 +1,28 @@
+import math 
+
+def find_pair(data, target):
+    pairs = {}
+    for d in data:
+        if d in pairs:
+            if pairs[d] != None:
+                yield tuple(sorted((d, target-d)))
+            pairs[target-d] = None
+        elif d not in pairs.values():
+            pairs[target-d] = d
+
+def find_triplet(data, target):
+    triplets = set()
+    for d in data:
+        for p1, p2 in find_pair(data, target-d):
+            t = tuple(sorted((d, p1, p2)))
+            if t not in triplets:
+                yield t
+                triplets.add(t)
+
+with open('input.txt', 'r') as file:
+    data = list(map(int, file.read().strip().split('\n')))
+
+print(*(k[0]*k[1] for k in find_pair(data, 2020)))
+
+
+print(*(k[0]*k[1]*k[2] for k in find_triplet(data, 2020)))
