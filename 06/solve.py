4356c036d05ea87d7ac9eab6f73159bcaad31351
Simon Bouchard <simon.bouchard23@gmail.com>
2020-12-06 13:41:18 -0500

    solve day 6

---
+++ 06/solve.py
@@ -0,0 +1,9 @@
+
+with open('input.txt', 'r') as file:
+    data = [d.strip().split() for d in file.read().split('\n\n')]
+
+# Part 1
+print(sum([len(set(answer for people in decl for answer in people)) for decl in data]))
+
+# Part 2
+print(sum(len(set.intersection(*map(set, decl))) for decl in data))
